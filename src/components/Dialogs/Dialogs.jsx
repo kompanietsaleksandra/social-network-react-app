70cf0ae8655501aca843f07488a08045b4c568c3
Aleksandra <53712472+kompanietsaleksandra@users.noreply.github.com>
2021-03-16 19:02:24 +0200

    add event on button

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,28 +6,8 @@ import DialogItem from "./DialogItem/DialogItem";
 
 const Dialogs = (props) => {
 
-    // let dialogsData =
-    //     [
-    //         {id: 1, name: 'Deny'},
-    //         {id: 2, name: 'Alisa'},
-    //         {id: 3, name: 'Kira'},
-    //         {id: 4, name: 'Greg'},
-    //         {id: 5, name: 'Jony'},
-    //         {id: 6, name: 'Ann'},
-    //     ];
-
     let dialogsElements = props.dialogsData
         .map (dialog => <DialogItem name={dialog.name} id={dialog.id} /> );
-
-    // let messagesData =
-    //     [
-    //         {id: 1, message: 'Hi All!!!'},
-    //         {id: 2, message: 'How is you doing, bro?'},
-    //         {id: 3, message: 'I like you)'},
-    //         {id: 4, message: 'Wooow, hi sweety!'},
-    //         {id: 5, message: 'Whats up??'},
-    //         {id: 6, message: 'Good morning, world!!'},
-    //     ];
 
     let messagesElements = props.messagesData
         .map( message => <Message message={message.message}/> );
--- src/index.js
+++ src/index.js
@@ -1,42 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {rerenderEntireTree} from './render';
 import state from './redux/state';
 
-// let dialogsData =
-//     [
-//         {id: 1, name: 'Deny'},
-//         {id: 2, name: 'Alisa'},
-//         {id: 3, name: 'Kira'},
-//         {id: 4, name: 'Greg'},
-//         {id: 5, name: 'Jony'},
-//         {id: 6, name: 'Ann'},
-//     ];
-// let messagesData =
-//     [
-//         {id: 1, message: 'Hi All!!!'},
-//         {id: 2, message: 'How is you doing, bro?'},
-//         {id: 3, message: 'I like you)'},
-//         {id: 4, message: 'Wooow, hi sweety!'},
-//         {id: 5, message: 'Whats up??'},
-//         {id: 6, message: 'Good morning, world!!'},
-//     ];
-//
-// let posts =
-//     [
-//         {id: 1, message: 'Hi, how are you?', likesCount: '12'},
-//         {id: 2, message: 'Its me, hi all', likesCount: 40},
-//         {id: 3, message: 'Hello, my name is Dany!', likesCount: 7},
-//     ];
+// addPost('Hohohohohoh');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App state={state} posts={state.posts} messagesData={state.messagesData} dialogsData={state.dialogsData}/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+rerenderEntireTree(state);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
